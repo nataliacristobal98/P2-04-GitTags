8bd52dc9d8723bac047f380c58ea12aa0dac26e4
Natalia Cristóbal <natalia.cristobal1@educa.madrid.org>
2020-11-11 20:07:07 +0100

    Cambiado NuevaClase y actualizado README

--- Practica3/src/NuevaClase.java
+++ Practica3/src/NuevaClase.java
@@ -1,5 +1,7 @@
 public class NuevaClase {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Linea añadida para v1.1");
+
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 P02-04 Git tags, localización de versiones
 
+ -Añadidos archivos a carpetaCasa y tag realizado (v1.0)
+ -Cambios realizado en NuevaClase y tag realizado (v1.1)
